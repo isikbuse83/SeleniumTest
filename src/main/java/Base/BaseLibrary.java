0c27b5e5ddf8c44850b3e965f8e41a42478549b0
isikbuse83 <isikbuse83@gmail.com>
2025-02-20 19:41:53 +0300

    add new cases

--- src/main/java/Base/BaseLibrary.java
+++ src/main/java/Base/BaseLibrary.java
@@ -1,6 +1,13 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseLibrary extends Data {
+
+    public static WebDriver driver;
 
     public void sleep (int millis) {
         try {
@@ -9,4 +16,11 @@ public class BaseLibrary extends Data {
             throw new RuntimeException(e);
         }
     }
+
+    public void switchTab(int index)
+    {
+        List<String> Tabs= new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(Tabs.get(index));
+    }
+
 }
